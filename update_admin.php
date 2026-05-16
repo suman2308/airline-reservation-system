652d36ace6c7b2a7357ac9f29d77ea0baad7d0e9
Suman Jash <iamsumanjash@gmail.com>
2026-05-17 02:40:15 +0530

    Remove temp file

--- update_admin.php
+++
@@ -1,5 +0,0 @@
-<?php
-$conn = mysqli_connect('localhost', 'root', '', 'aerobook_db');
-$hash = password_hash('admin123', PASSWORD_DEFAULT);
-mysqli_query($conn, "UPDATE admins SET password='$hash' WHERE username='admin'");
-echo "Updated!";
